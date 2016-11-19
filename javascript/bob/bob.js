538fa9653eb750191feaece840b7a08ab66590f4
codeangler <dev@codeangler.com>
2016-11-19 07:59:10 -0700

    solution

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -5,26 +5,43 @@
 
 var Bob = function() {};
 
+
+//  a great solution  http://exercism.io/submissions/d3e2be7ca1db4bc38806cbe492bb7674
 Bob.prototype.hey = function(input) {
-  let punct = input.slice(-1);
-  let lastLetter = input.slice(-2);
-  var lastLetterIsCapital = /^[A-Z]/.test(lastLetter);
-
-  if (!lastLetterIsCapital && punct === '!') {
-    punct = "."
-  } 
-
-  const output = {
-    '.'            : "Whatever.",
-    undefined      : "Whatever.",
-    '!'            : "Whoa, chill out!",
-    '?'            : "Sure."
+  if (input.match(/[a-zA-Z]+/) != null & input == input.toUpperCase()){
+    return "Whoa, chill out!";
   }
-    console.log(output[punct])
-    return output[punct]
+  else if (input.match(/\?$/) !== null) {
+    return "Sure.";
+  }  
+  else if(input.match(/^\s*$/) !== null){
+    return 'Fine. Be that way!';
+  }
+  return "Whatever.";
+};
+
+
+// My failure
+// Bob.prototype.hey = function(input) {
+//   let punct = input.slice(-1);
+//   let lastLetter = input.slice(-2);
+//   var lastLetterIsCapital = /^[A-Z]/.test(lastLetter);
+
+//   if (!lastLetterIsCapital && punct === '!') {
+//     punct = "."
+//   } 
+
+//   const output = {
+//     '.'            : "Whatever.",
+//     undefined      : "Whatever.",
+//     '!'            : "Whoa, chill out!",
+//     '?'            : "Sure."
+//   }
+//     console.log(output[punct])
+//     return output[punct]
  
 
-  // console.log(results)
-};
+//   // console.log(results)
+// };
 
 module.exports = Bob;
